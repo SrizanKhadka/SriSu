8ed723ea96dd041a871949039d42215cfd032430
SrizanKhadka <khadkasrijan785@gmail.com>
2024-12-22 19:58:09 +0545

    updated todo.

--- chat/tests.py
+++ chat/tests.py
@@ -17,7 +17,7 @@ async def test_websocket():
         print(f"Sent: {message}")
 
         # Receive a response
-        # response = await websocket.recv()
-        # print(f"Received: {response}")
+        response = await websocket.recv()
+        print(f"Received: {response}")
 
 asyncio.run(test_websocket())
