e04d2ebea625469e501c01f35f429c4d0ec4f430
SrizanKhadka <khadkasrijan785@gmail.com>
2025-01-12 13:34:17 +0545

    project steup and bug on websocket fixed.

--- chat/tests.py
+++ chat/tests.py
@@ -1,17 +1,30 @@
 import asyncio
 import websockets
 import json
+import base64
+
+
+def encode_file_to_base64(file_path):
+    try:
+        with open(file_path, "rb") as file:
+            return base64.b64encode(file.read()).decode("utf-8")
+    except FileNotFoundError:
+        print("File not found. Check the file path.")
+        return None
+
 
 async def test_websocket():
     uri = "ws://127.0.0.1:8000/ws/chat/1/"
     async with websockets.connect(uri) as websocket:
         # Send a test message
+        image_path = r"C:\Users\acer\Downloads\webtest.png"
+        image_base64 = encode_file_to_base64(image_path)
         message = {
             "text": "Hello from Python WebSocket client!",
             "message_type": "IMAGE",
-            "sender_id": 5,
-            "media": "/home/srizan/Pictures/Screenshots/",  # Add file path if testing media uploads
-            "reply_to": None  # Add message ID if replying to a message
+            "sender_id": 25,
+            "media": image_base64,  # Add file path if testing media uploads
+            "reply_to": None,  # Add message ID if replying to a message
         }
         await websocket.send(json.dumps(message))
         print(f"Sent: {message}")
@@ -19,5 +32,6 @@ async def test_websocket():
         # Receive a response
         response = await websocket.recv()
         print(f"Received: {response}")
+
 
 asyncio.run(test_websocket())
