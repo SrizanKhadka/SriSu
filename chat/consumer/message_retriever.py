fa504e7d1e4c9f0917733993b8d5584c2ca8edd7
SrizanKhadka <khadkasrijan785@gmail.com>
2025-12-03 21:06:37 +0545

    feat: message ordering done.

--- chat/consumer/message_retriever.py
+++ chat/consumer/message_retriever.py
@@ -15,7 +15,7 @@ class ChatMessagePagination(PageNumberPagination):
 
 
 async def get_paginated_messages(chat_room, user, page, page_size):
-    queryset = MessageModel.objects.filter(chat_room=chat_room).order_by("timestamp")
+    queryset = MessageModel.objects.filter(chat_room=chat_room).order_by("-timestamp")
 
     queryset = queryset.exclude(
         Q(delete_for__user__contains=[{"user_id": user.id, "delete_option": DeleteOption.DELETE_FOR_ME}])
